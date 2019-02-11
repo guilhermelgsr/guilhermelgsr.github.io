1006427a9b980c8abaede5970a8a12e51669d013
guilhermelgsr <guilhermelgsr@gmail.com>
2019-02-11 20:25:39 -0200

    php index criado

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+
+<title>Favoritos</title>
+
+<body>
+<?php
+	$titles = ('Alura', 'Angular', 'BP', 'CPFL', 'Drive', 'Gmail', 'GitLab');
+	echo "<ul>";
+	foreach($title as $titles) {
+	    echo "<li>$current</li>";
+	}
+	echo "</ul>";
+?>
+
+<!-- 
+<ul>
+<li>
+<a href="alura.com.br">Alura</a>
+<a href="alura.com.br">Angular</a>
+<a href="">Angu</a>
+<a href=""></a>
+
+</li>
+</ul> -->
+
+<body>
+
+</html>
